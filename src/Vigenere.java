f5db030faa4d7d6cf8aae34fe6f02449ad9a7d02
Steve <stevenlyall@gmail.com>
2016-02-23 19:31:01 -0800

    argument validation fixed

--- src/Vigenere.java
+++ src/Vigenere.java
@@ -1,33 +1,76 @@
 public class Vigenere {
 
     public static void main(String[] args) {
-        if (!validateArguments(args)) {
+        Vigenere vigenere = new Vigenere();
+        if (!vigenere.validateArguments(args)) {
             return;
         }
-    System.out.println("Vigenere Cypher");
+
+        System.out.println("Vigenere Cypher");
+
+        if (args[0].equals("-e")) {
+            String key = args[1];
+            String filePath = args[2];
+            vigenere.encrypt(key, filePath);
+        }
+        if (args[0].equals("-d")) {
+            String key = args[1];
+            String filePath = args[2];
+            vigenere.decrypt(key, filePath);
+        }
+        if (args[0].equals("-b")) {
+            String filePath = args[1];
+            vigenere.breakCipher(filePath);
+        }
+
     }
 
-    private static boolean validateArguments(String[] args) {
+    private void breakCipher(String filePath) {
+        System.out.println("Breaking cipher in file " + filePath);
+    }
+
+    private void decrypt(String key, String filePath) {
+        System.out.println("Decrypting file " + filePath + " with key " + key);
+    }
+
+    private void encrypt(String key, String filePath) {
+        System.out.println("Encrypting file " + filePath + " with key " + key);
+    }
+
+    private boolean validateArguments(String[] args) {
+
         if (args.length < 2 || args.length > 3) {
-            System.err.println("Error: Incorrect arguments supplied.");
-            System.err.println("Usage: Vigenere <mode> [key] <file>");
-            System.err.println("\t-e\t<key> <file>\t encrypt a file using the supplied key");
-            System.err.println("\t-d\t<key> <file>\t decrypt a file using the supplied key");
-            System.err.println("\t-b\t<file>      \t attempt to break the cipher");
+            printHelpText();
             return false;
         }
-        if (args[0] != "-e" || args[0]!= "-d" || args[0] != "-b") {
-            System.err.println("Error: Must specify correct mode. -e for encrypt, -d to decrypt, -b to break");
-            System.err.println("Usage: Vigenere <mode> [key] <file>");
-            System.err.println("\t-e\t encrypt the file");
-            System.err.println("\t-d\t decrypt the file");
+
+        boolean validFlag = (args[0].equals("-e") || args[0].equals("-d" ) || args[0].equals("-b"));
+
+        if (!validFlag)  {
+            printHelpText();
             return false;
         }
-        if (args[1] == null) {
-            System.err.println("Error: Must supply key for decryption and encryption");
-            System.err.println("Usage: Vigenere <mode> [key] <file>");
+
+        if ((args[0].equals("-e") || args[0].equals("-d")) && args.length != 3) {
+            System.err.println("Error: Must supply key and file for decryption or encryption");
+            System.err.println("Usage: Vigenere <mode> [<key>] <file>");
             return false;
         }
+
+        if (args[0].equals("-b") && args.length != 2) {
+            System.err.println("Error: Must supply file name to break cipher");
+            System.err.println("Usage: Vigenere <mode> <file>");
+            return false;
+        }
+
         return true;
+    }
+
+    private void printHelpText() {
+        System.err.println("Error: Must specify correct mode. -e for encrypt, -d to decrypt, -b to break");
+        System.err.println("Usage: Vigenere <mode> [<key>] <file>");
+        System.err.println("\t-e\t<key> <file>\t encrypt a file using the supplied key");
+        System.err.println("\t-d\t<key> <file>\t decrypt a file using the supplied key");
+        System.err.println("\t-b\t<file>      \t attempt to break the cipher");
     }
 }
